b3e5c3a3459676094e5133daad409b7569300840
Kaiser Styve <72132226+ksthecrowned@users.noreply.github.com>
2021-07-05 05:38:50 +0100

    Added new files

---
+++ js/main.js
@@ -0,0 +1,114 @@
+
+
+// fancy animation
+// (function() {
+//     const text = document.querySelector('.fancy');
+//     const strText = text.textContent;
+//     const splitText = strText.split("");
+
+//     text.textContent = "";
+//     for(let i = 0; i < splitText.length; i++) {
+//         text.innerHTML += '<span class="fancy_s">' + splitText[i] + '</span>';
+//     }
+
+//     let char = 0;
+//     let timer = setInterval(onTick, 50);
+
+//     function onTick() {
+//         const span = document.querySelectorAll('span .fancy_s')[char];
+//         span.classList.add('fade');
+//         char++
+
+//         if(char === splitText.length) {
+//             complete();
+//             return;
+//         }
+//     };
+
+//     function complete() {
+//         clearInterval(timer);
+//         timer = null;
+//     };
+// })();
+
+function autoType(elementClass, typingSpeed){
+    var thhis = $(elementClass);
+    thhis.css({
+      "position": "relative",
+      "display": "inline-block"
+    });
+    thhis.prepend('<div class="cursor" style="right: initial; left:0;"></div>');
+    thhis = thhis.find(".text-js");
+    var text = thhis.text().trim().split('');
+    var amntOfChars = text.length;
+    var newString = "";
+    thhis.text("|");
+    setTimeout(function(){
+      thhis.css("opacity",1);
+      thhis.prev().removeAttr("style");
+      thhis.text("");
+      for(var i = 0; i < amntOfChars; i++){
+        (function(i,char){
+          setTimeout(function() {        
+            newString += char;
+            thhis.text(newString);
+          },i*typingSpeed);
+        })(i+1,text[i]);
+      }
+    },1500);
+  }
+  
+  $(document).ready(function(){
+    // Now to start autoTyping just call the autoType function with the 
+    // class of outer div
+    // The second paramter is the speed between each letter is typed.   
+    autoType(".type-js", 100);
+    autoType(".type-js2", 100);
+  
+    
+  });
+
+
+// Filtering works
+filterSelection("all")
+function filterSelection(c) {
+    var x, i;
+    x = document.getElementsByClassName("filterDiv");
+    if (c == "all") c = "";
+    for (i = 0; i < x.length; i++) {
+    w3RemoveClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+    }
+}
+
+function w3AddClass(element, name) {
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
+    }
+}
+
+function w3RemoveClass(element, name) {
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+        arr1.splice(arr1.indexOf(arr2[i]), 1);     
+    }
+    }
+    element.className = arr1.join(" ");
+}
+
+// Add active class to the current button (highlight it)
+var btnContainer = document.getElementById("myBtnContainer");
+var btns = btnContainer.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function(){
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+    });
+}
